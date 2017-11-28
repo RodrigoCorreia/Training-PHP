38d5cbf3c413f455a186e3cebb913a43a53c8e5a
Rodrigo Correia <rodrigocorrei@gmail.com>
2017-11-28 14:11:10 -0300

    Using Library GD

---
+++ GD/exemplo-01.php
@@ -0,0 +1,17 @@
+<?php 
+
+header("Content-Type: image/png");
+
+$image = imagecreate(256, 256);
+
+$black = imagecolorallocate($image, 0, 0, 0);
+$red = imagecolorallocate($image, 255, 0, 0);
+
+
+imagestring($image, 5, 30, 120, "Rodrigo Treinamento PHP", $red);
+
+imagepng($image);
+
+imagedestroy($image);
+
+ ?>
