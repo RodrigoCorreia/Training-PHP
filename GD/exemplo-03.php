1fc1f65744e650a38e1771dcaddbfc37d0b72250
Rodrigo Correia <rodrigocorrei@gmail.com>
2017-11-28 14:57:47 -0300

    Using fonts

---
+++ GD/exemplo-03.php
@@ -0,0 +1,20 @@
+<?php 
+
+$image = imagecreatefromjpeg("Images/certificado.jpg");
+
+$titleColor = imagecolorallocate($image, 0, 0, 0);
+$gray = imagecolorallocate($image, 100, 100, 100);
+
+imagettftext($image, 32, 0, 320, 250, $titleColor, "fonts".DIRECTORY_SEPARATOR."Bevan".DIRECTORY_SEPARATOR."Bevan-Regular.ttf", "CERTIFICADO");
+imagettftext($image, 32, 0, 375, 350, $titleColor, "fonts".DIRECTORY_SEPARATOR."Playball".DIRECTORY_SEPARATOR."Playball-Regular.ttf", "Rodrigo Correia");
+imagestring($image, 5, 440, 370, utf8_decode("Concluído: ") . date("d/m/Y"), $titleColor);
+
+
+header("Content-type: image/jpeg"); //Importante comentar essa linha para perceber os erros
+
+imagejpeg($image);
+
+imagedestroy($image);
+
+
+ ?>
